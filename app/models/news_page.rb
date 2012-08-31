8d0811eadb193cafec13c3b8d288040571540f22
Sam Whited <sam@samwhited.com>
2012-08-30 20:05:05 -0400

    Respect `defaults.page.status'

--- app/models/news_page.rb
+++ app/models/news_page.rb
@@ -6,6 +6,7 @@ class NewsPage < Page
     page.parts.concat(self.default_page_parts)
     page.fields.concat(self.default_page_fields)
     page.parent_id = Page.find_by_path(NewsExtension.news_paths[0]).try(:id)
+    page.status = Status[Radiant::Config['defaults.page.status']]
     page
   end
 
